180219397b1da06d2a8eb74cef171cccd4c12cd7
cryharry <cryharryk@gamil.com>
2014-07-18 18:08:59 +0900

    2014-7-17 자바수업

---
+++ src/Hello.java
@@ -0,0 +1,28 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		//실행 (ctrl+F11)
+		//한줄주석:설명문 (ctrl+shift+c)
+		
+		/* 여러줄 줄석 (ctrl+shitf+/)
+		 여러줄 계속	 */
+		
+		/*
+		 * 주석 -> 문서화
+		 */
+
+		System.out.println("Hello Java!!");
+		
+		// 상수, 변수, 자료형
+		// 연산자
+		// 제어문(조건문,반복문) - 알고리즘
+		// 정보처리기능사,산업기사,기사 - 필기,실기
+		// 배열 , 함수(메서드)
+		// 객체지향개념
+		// API (String, 컬렉션 ...)
+		// 예외처리, 스레드
+		// 자바 입,출력
+		// AWT, Swing 화면 -> 기능
+		// 데이터베이스
+	}
+}
